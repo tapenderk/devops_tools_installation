eedafecab57abf87a1caac29591d8c29724559f1
Tapender Kamboj <tapenderkamboj@gmail.com>
2024-04-29 14:25:41 +0530

    Fix the current installations issues

--- auto-install.sh
+++ auto-install.sh
@@ -5,6 +5,7 @@ toolToInstall=("jenkins" "Maven" "trivy" "docker" "sonarqube")
 for tools in "${toolToInstall[@]}"
 do
    echo -e "\n ====    Installing ${tools}    ==== \n "
+   chmod +x ${tools}.sh
    ./${tools}.sh
    echo -e " \n ==== Done Installing ${tools} ==== \n "
 done
--- trivy.sh
+++ trivy.sh
@@ -1,7 +1,7 @@
 # A Simple and Comprehensive Vulnerability Scanner for Containers and other Artifacts, Suitable for CI.
 
-sudo apt-get install wget apt-transport-https gnupg lsb-release
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update
-sudo apt-get install trivy
+sudo apt-get update -y
+sudo apt-get install trivy -y
